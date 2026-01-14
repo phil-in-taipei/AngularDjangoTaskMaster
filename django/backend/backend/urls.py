4b4a97d3891373b1570af33c052d1000d66dce11
phil-in-taipei <sweeneyphilip11@gmail.com>
2026-01-14 12:44:04 +0800

    Finished setup for user profile endpoints and serializers and updated requirements for simple jwt -- django/backend

--- django/backend/backend/urls.py
+++ django/backend/backend/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, re_path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/', include('authapp.urls')),
+    path('api/profiles/', include('user_profiles.urls')),
 ]
